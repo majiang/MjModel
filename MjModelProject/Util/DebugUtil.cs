a861ddb504af05430c9ebfc8232e952f0e03f6b4
rick0000 <rickdev0000@gmail.com>
2015-10-12 20:43:40 +0900

    名前空間変更

---
+++ MjModelProject/Util/DebugUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MjModelProject
+{
+    public static class DebugUtil
+    {
+        public static void ServerDebug(string msg)
+        {
+            Debug.WriteLine("Server:"+msg);
+
+        }
+
+    }
+}
