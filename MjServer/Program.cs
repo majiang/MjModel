a2d22235b1d0d11788f5e23e36450c0c49386117
rick0000 <rickdev0000@gmail.com>
2016-04-03 22:30:55 +0900

    add new server

---
+++ MjServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MjServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set Confing
+            // Make ServerFacerd Instance
+            // StartServer
+        }
+    }
+}
