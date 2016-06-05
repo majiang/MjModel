a7185747194076f979067205845a80b74a14f7b9
rick_t460s <rickdev0000@gmail.com>
2016-06-05 21:25:26 +0900

    Add one game play client

--- MjClient/Program.cs
+++ MjClient/Program.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MjClient
 {
     class Program
     {
+        readonly static int ALL_GAME_NUM = 100;
         static void Main(string[] args)
         {
-            
+            for (int i = 0; i < ALL_GAME_NUM; i++)
+            {
+                Console.WriteLine("start game : " + i);
+                DoOneGame();
+                Console.WriteLine("end game : " + i);
+            }
+        }
+
+
+        static void DoOneGame()
+        {
+            var roomName = "testRoom";
+            ClientFacade cf0 = new ClientFacade("player0", roomName);
+
+
+            //cf0.SetLoggable(true);
+
+            cf0.StartClient();
+
+            while (cf0.IsEndGame == false)
+            {
+                Thread.Sleep(10);
+            }
         }
     }
 }
